43857c5c82364cca48c1f09cc3f4d07c59152853
nindanaoto <matsuoka.kotaro@gmail.com>
2026-09-09 02:19:20 +0900

    Validate compact RAM emission and expose compact reset regression

--- src/test/scala/ngen/KyberSystemVerilogSpec.scala
+++ src/test/scala/ngen/KyberSystemVerilogSpec.scala
@@ -12,3 +12,12 @@ class KyberSystemVerilogSpec extends AnyFunSuite:
     assert(rtl.contains("output reg done"))
     assert(rtl.contains("KYBER_QINV = 16'd3327"))
     assert(!rtl.contains(" % "))
+
+  test("compact Kyber validates its decoder and emits compilable physical RAM banks"):
+    import java.nio.file.Files
+    import scala.sys.process.*
+    val rtl=KyberSystemVerilog.emit(banked=true)
+    assert(!rtl.contains("reg [11:0] work ["))
+    val directory=Files.createTempDirectory("ngen-kyber-banked")
+    Files.writeString(directory.resolve("KyberHPM1PE.v"),rtl)
+    assert(Process(Seq("iverilog","-g2012","-s","KyberHPM1PE","-o","simulation","KyberHPM1PE.v"),directory.toFile).! == 0)
